cc87733de6fd0a976511e96fdfce4dbf708b6154
gbhx <fordo3@icloud.com>
2025-06-22 11:54:03 +0800

    h

---
+++ encrypt.js
@@ -0,0 +1,40 @@
+
+function change() {
+
+
+    const value = document.getElementById("inputText").value.toLowerCase();
+    const output = document.getElementById("output");
+    const key = parseInt(document.getElementById("key").value);
+
+
+
+
+    let alph = "123456789abcdefghijklmnopqrstuvwxyz";
+    const map = {};
+    let result = "";
+
+    for(let i=0; i<alph.length; i++) {
+        map[alph[i]] = i + key;
+        
+        if (map[alph[i]] > 34){
+            map[alph[i]] -= 35;
+        }
+    }
+    
+    
+
+    for(let i=0; i < value.length; i++) {
+        let char = value[i];
+
+        if (char !== " "){
+            result += alph[map[value[i]]];
+    } else {
+        result += " ";
+    }
+
+}
+output.innerHTML = result;
+}
+
+
+
